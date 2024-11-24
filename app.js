3a6a7b8ba0800631acd30609a6269a587e6bf9fa
vteam27 <tiwarivaibhavin@gmail.com>
2024-11-24 22:12:19 +0530

    added chat history

--- app.js
+++ app.js
@@ -52,19 +52,32 @@ app.post('/uploadFiles', async (req, res) => {
   res.send({ message: 'Files added to database successfully' });
 });
 
+let chatHistory = []; 
+
 app.post('/chat', async (req, res) => {
   console.log("query receiced!");
   const { query } = req.body;
+  chatHistory.push(`User: ${query}`);
   const collection = await getOrCreateCollection("Papers");
   const dbres = await queryCollection(collection, 5, [query]);
   let context=dbres.documents[0];
   context = JSON.stringify(context)
-  const final_query = `You are an question answer assistant bot for giving information on a Research paper management system. If you don\'t know the answer, just say that you don\'t know. Don't output anything other than answering the asked question. Use markup language to format your answer. Context: \n\n ${context}. Always include metadata information on authors, publications, year, link to the papers, etc if relevant.  Question: ${query} `
+  const history = chatHistory.join("\n");
+  const final_query = `You are an question answer assistant chatbot for giving information on a Research paper management system. 
+  If you don\'t know the answer, just say that you don\'t know. Don't output anything other than answering the asked question.
+  Use markup language to format your answer. 
+  Chat History: ${history} 
+  Context: \n\n ${context}. 
+  Always include metadata information on authors, publications, year, link to the papers, etc if relevant.  
+  
+  Question: ${query} `
   console.log(final_query);
 
   const result= await model.generateContent(final_query);
   const fres=result.response.text();
   console.log(fres);
+  chatHistory.push(`Bot: ${fres}`);
+
 
   res.send({ ans: fres});
 });
